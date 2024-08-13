c3528d514aa683e72f8fc530711a86878269a7ef
zhangguokai <zhangguokai@inspur.com>
2024-08-13 16:12:11 +0800

    feat: add rdbc_value get_string for vec and map

--- crates/bmbp_rdbc_type/src/rdbc_value.rs
+++ crates/bmbp_rdbc_type/src/rdbc_value.rs
@@ -16,6 +16,50 @@ pub enum RdbcValue {
     Null,
 }
 
+impl RdbcValue {
+    pub fn get_string(&self) -> String {
+        match self {
+            RdbcValue::Int(i) => i.to_string(),
+            RdbcValue::BigInt(i) => i.to_string(),
+            RdbcValue::Float(i) => i.to_string(),
+            RdbcValue::BigFloat(i) => i.to_string(),
+            RdbcValue::String(i) => i.to_string(),
+            RdbcValue::DateTime(i) => i.to_string(),
+            RdbcValue::Bool(i) => i.to_string(),
+            RdbcValue::Null => "".to_string(),
+            RdbcValue::Vec(v) => {
+                "".to_string()
+            }
+            RdbcValue::Map(v) => { "".to_string() }
+        }
+    }
+    pub fn get_isize(&self) -> Option<isize> {
+        match self {
+            RdbcValue::Int(i) => Some(i.clone() as isize),
+            RdbcValue::BigInt(i) => Some(i.clone() as isize),
+            RdbcValue::Float(i) => Some(i.clone() as isize),
+            RdbcValue::BigFloat(i) => Some(i.clone() as isize),
+            _ => None,
+        }
+    }
+    pub fn get_usize(&self) -> Option<usize> {
+        match self {
+            RdbcValue::Int(i) => Some(i.clone() as usize),
+            RdbcValue::BigInt(i) => Some(i.clone() as usize),
+            RdbcValue::Float(i) => Some(i.clone() as usize),
+            RdbcValue::BigFloat(i) => Some(i.clone() as usize),
+            _ => None,
+        }
+    }
+}
+
+impl Display for RdbcValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = self.get_string();
+        write!(f, "{}", str)
+    }
+}
+
 impl From<i8> for RdbcValue {
     fn from(i: i8) -> RdbcValue {
         RdbcValue::Int(i as i16)
@@ -223,45 +267,6 @@ impl From<&str> for RdbcValue {
     }
 }
 
-impl RdbcValue {
-    pub fn get_string(&self) -> String {
-        match self {
-            RdbcValue::Int(i) => i.to_string(),
-            RdbcValue::BigInt(i) => i.to_string(),
-            RdbcValue::Float(i) => i.to_string(),
-            RdbcValue::BigFloat(i) => i.to_string(),
-            RdbcValue::String(i) => i.to_string(),
-            RdbcValue::DateTime(i) => i.to_string(),
-            RdbcValue::Bool(i) => i.to_string(),
-            RdbcValue::Null => "".to_string(),
-        }
-    }
-    pub fn get_isize(&self) -> Option<isize> {
-        match self {
-            RdbcValue::Int(i) => Some(i.clone() as isize),
-            RdbcValue::BigInt(i) => Some(i.clone() as isize),
-            RdbcValue::Float(i) => Some(i.clone() as isize),
-            RdbcValue::BigFloat(i) => Some(i.clone() as isize),
-            _ => None,
-        }
-    }
-    pub fn get_usize(&self) -> Option<usize> {
-        match self {
-            RdbcValue::Int(i) => Some(i.clone() as usize),
-            RdbcValue::BigInt(i) => Some(i.clone() as usize),
-            RdbcValue::Float(i) => Some(i.clone() as usize),
-            RdbcValue::BigFloat(i) => Some(i.clone() as usize),
-            _ => None,
-        }
-    }
-}
-
-impl Display for RdbcValue {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let str = self.get_string();
-        write!(f, "{}", str)
-    }
-}
 
 impl From<&RdbcValue> for String {
     fn from(value: &RdbcValue) -> Self {
@@ -280,6 +285,8 @@ impl From<&RdbcValue> for bool {
             RdbcValue::String(s) => s.clone() != "",
             RdbcValue::DateTime(_) => false,
             RdbcValue::Null => false,
+            RdbcValue::Vec(v) => v.is_empty(),
+            RdbcValue::Map(v) => v.is_empty()
         }
     }
 }
