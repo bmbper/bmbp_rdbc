53406d82657fc91709168552741f953ac721f4a9
zhangguokai <zhangguokai@inspur.com>
2024-09-22 21:10:46 +0800

    feat: 发布脚本

---
+++ publish.sh
@@ -0,0 +1,19 @@
+echo "publish bmbp_rdbc..."
+cd crates
+cd bmbp_rdbc_type
+cargo publish
+cd ../bmbp_rdbc_sql 
+cargo publish
+cd ../bmbp_rdbc_orm
+cargo publish
+cd ../bmbp_rdbc_script
+cargo publish
+cd ../bmbp_rdbc_marco
+cargo publish
+cd ../bmbp_rdbc_driver
+cargo publish
+cd ../bmbp_rdbc_adapter
+cargo publish
+cd ../bmbp_rdbc
+cargo publish
+echo "publish bmbp_rdbc success"
