9331a8ddc2bc6cb0324a78b94e29166f42af1859
zhangguokai <zhangguokai@inspur.com>
2025-01-29 21:22:01 +0800

    fix: 构建事务成功

--- crates/bmbp_rdbc_sql/src/render/query.rs
+++ crates/bmbp_rdbc_sql/src/render/query.rs
@@ -1,8 +1,5 @@
 use crate::render::render::RdbcSqlRender;
 use crate::types::RdbcQuery;
-use bmbp_rdbc_type::RdbcValue;
-use std::collections::HashMap;
-
 pub struct RdbcQueryRender {
     query: RdbcQuery,
 }
